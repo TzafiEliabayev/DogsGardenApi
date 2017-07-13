33acebd2af2d9f663acaa891bba07b36af80640b
Tzafi Eliabayev <tzafi.eli@gmail.com>
2017-07-13 16:09:41 +0300

    initial commit

---
+++ gardens.js
@@ -0,0 +1,26 @@
+var education = {
+    "schools": [
+    {
+        "name": "Academit Tel-Aviv - Yaffo",
+        "location": "Yaffo",
+        "degree": "BSc",
+        "dates": "2012-2016",
+        "url": "",
+        "majors": ["Computer Science"]
+    },
+    {
+        "name": "Hebrew University",
+        "location": "Rehovot",
+        "degree": "BSc",
+        "dates": "2009-2011",
+        "url": "",
+        "majors": ["Animal Science"]
+    }],
+    "onlineCourses": [
+    {
+        "title": "WEB Developing",
+        "school": "Udacity",
+        "dates": "July-August 2017",
+        "url": ""
+    }]
+}
